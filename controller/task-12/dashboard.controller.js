e89ada795d286407907f15c67f4ee6e69f4419b6
yashvi-ghetiya <yashvi.ghetiya.2024@gmail.com>
2024-04-03 10:31:05 +0530

    April 3 10:30

--- controller/task-12/dashboard.controller.js
+++ controller/task-12/dashboard.controller.js
@@ -6,7 +6,7 @@ dashboard.get('/dashboard', async function (req, res) {
     console.log(await authentication(req));
     if(await authentication(req))
     {
-        console.log("true");
+       
       var userName = await executeQuery('combinedTask', `select firstname,lastname from users_task12 where id=${getUserId(req)} and status=1;`);
         
         res.render('./task-12/html/dashboard',{firstname:userName[0]['firstname'],lastname:userName[0]['lastname']});
